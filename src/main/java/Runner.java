1d33c949d9213b0b3c7bcbf413e1385ec49328ee
Kyle Avrett <kaavrett@gmail.com>
2015-10-28 21:35:48 +0000

    Fixing nuked commit

---
+++ src/main/java/Runner.java
@@ -0,0 +1,43 @@
+
+import java.awt.EventQueue;
+
+public class Runner {
+    public static void main(String[] args) {
+        PizzaSystem system = new PizzaSystem();
+        
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    ManagerGui window = new ManagerGui(system);
+                    window.frmManagerGui.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        
+        
+        
+        // EventQueue.invokeLater(new Runnable() {
+        // public void run() {
+        // try {
+        // KioskGUI window = new KioskGUI(system);
+        // window.frmKiosk.setVisible(true);
+        // } catch (Exception e) {
+        // e.printStackTrace();
+        // }
+        // }
+        // });
+        
+        // EventQueue.invokeLater(new Runnable() {
+        // public void run() {
+        // try {
+        // ChefGUI window = new ChefGUI(system);
+        // window.frmOrdersToCook.setVisible(true);
+        // } catch (Exception e) {
+        // e.printStackTrace();
+        // }
+        // }
+        // });
+    }
+}
